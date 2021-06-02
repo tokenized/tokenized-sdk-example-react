03787974efe8eebcbf6ced1e876c41813c4328a6
Jon Travers <Jon.Travers@gmail.com>
2021-06-02 11:06:00 +0100

    Step 3: Querying the assets with the SDK

--- src/features/tutorial/TutorialPage.js
+++ src/features/tutorial/TutorialPage.js
@@ -1,36 +1,55 @@
 import React from 'react';
+import {
+  usePrimaryVault,
+  useFilteredBalances,
+} from '@tokenized/sdk-react-private';
 
 export default function TutorialPage() {
+  const vault = usePrimaryVault();
+  const balances = useFilteredBalances(vault?.id, {
+    includeInactive: false,
+  });
+
   return (
     <section className="section">
       <h1 className="title">Tutorial sandbox</h1>
-      <div className="columns">
-        <div className="column is-half">
-          <div className="box">
-            <h2 className="title is-5">Asset summary</h2>
-            <div className="field is-grouped is-grouped-multiline">
-              <div className="control">
-                <div className="tags has-addons">
-                  <span className="tag is-dark">Asset 1</span>
-                  <span className="tag is-info">$11.11</span>
-                </div>
-              </div>
-              <div className="control">
-                <div className="tags has-addons">
-                  <span className="tag is-dark">Asset 2</span>
-                  <span className="tag is-success">$22.22</span>
-                </div>
-              </div>
-              <div className="control">
-                <div className="tags has-addons">
-                  <span className="tag is-dark">Asset 3</span>
-                  <span className="tag is-primary">$33.33</span>
-                </div>
+      {balances?.data && (
+        <div className="columns">
+          <div className="column is-half">
+            <div className="box">
+              <h2 className="title is-5">Asset summary</h2>
+              <div className="field is-grouped is-grouped-multiline">
+                {balances.data.map((balance) => {
+                  const quantity = balance.isLiability
+                    ? balance.quantities?.issuedLiability
+                    : balance.quantities?.value;
+                  let valueOrLiability;
+                  if (quantity?.displayCurrency) {
+                    valueOrLiability = new Intl.NumberFormat(
+                      undefined,
+                      quantity.displayCurrency.NumberFormatOptions,
+                    ).format(quantity.displayCurrency.number);
+                  }
+                  return (
+                    <div key={balance.assetId} className="control">
+                      <div className="tags has-addons">
+                        <span className="tag is-link">
+                          {balance?.assetName}
+                        </span>
+                        {valueOrLiability && (
+                          <span className="tag is-dark">
+                            {valueOrLiability}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 }
