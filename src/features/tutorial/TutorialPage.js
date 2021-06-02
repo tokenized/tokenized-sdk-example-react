cd2c14b4e2eff2c5e9452a5a68ebeb14d1fc0cd6
Jon Travers <Jon.Travers@gmail.com>
2021-06-02 10:53:44 +0100

    Step 2: Asset summary UI design

--- src/features/tutorial/TutorialPage.js
+++ src/features/tutorial/TutorialPage.js
@@ -4,6 +4,33 @@ export default function TutorialPage() {
   return (
     <section className="section">
       <h1 className="title">Tutorial sandbox</h1>
+      <div className="columns">
+        <div className="column is-half">
+          <div className="box">
+            <h2 className="title is-5">Asset summary</h2>
+            <div className="field is-grouped is-grouped-multiline">
+              <div className="control">
+                <div className="tags has-addons">
+                  <span className="tag is-dark">Asset 1</span>
+                  <span className="tag is-info">$11.11</span>
+                </div>
+              </div>
+              <div className="control">
+                <div className="tags has-addons">
+                  <span className="tag is-dark">Asset 2</span>
+                  <span className="tag is-success">$22.22</span>
+                </div>
+              </div>
+              <div className="control">
+                <div className="tags has-addons">
+                  <span className="tag is-dark">Asset 3</span>
+                  <span className="tag is-primary">$33.33</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
