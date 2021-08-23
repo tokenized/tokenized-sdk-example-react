661def5e1a7732196c57d4f29b1b874da968e0f9
Jon Travers <jon.travers@tokenized.com>
2021-08-23 21:42:47 +0100

    NEX-1661 Update activity page with all three tabs and using the new formatted data

--- src/features/activity/ActivityPage.js
+++ src/features/activity/ActivityPage.js
@@ -6,11 +6,53 @@ import {
   Redirect,
   useRouteMatch,
 } from 'react-router-dom';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, FormattedNumber } from 'react-intl';
+import { useActivity } from '@tokenized/sdk-react-private';
 import ActivityTable from './ActivityTable';
+
+// eslint-disable-next-line react/display-name
+const TabLink = React.forwardRef(
+  ({ className, style, children, navigate, ...props }, ref) => (
+    <li style={style} className={className}>
+      <a
+        ref={ref}
+        {...props}
+        onClick={(event) => {
+          event.preventDefault();
+          navigate();
+        }}
+      >
+        {children}
+      </a>
+    </li>
+  ),
+);
+
+const NavTab = (props) => {
+  return <NavLink {...props} component={TabLink} activeClassName="is-active" />;
+};
 
 export default function ActivityPage() {
   const { url, path } = useRouteMatch();
+
+  const openCount =
+    useActivity({
+      includeSuccessfulEvents: false,
+      includeIncompleteEvents: false,
+      includeFailedEvents: false,
+      includeExpiredEvents: false,
+      includeEventsRequiringAction: true,
+      includeEventsPendingOthers: false,
+    })?.data?.length || 0;
+  const pendingCount =
+    useActivity({
+      includeSuccessfulEvents: false,
+      includeIncompleteEvents: false,
+      includeFailedEvents: false,
+      includeExpiredEvents: false,
+      includeEventsRequiringAction: false,
+      includeEventsPendingOthers: true,
+    })?.data?.length || 0;
 
   return (
     <section className="section">
@@ -20,41 +62,61 @@ export default function ActivityPage() {
           description="Activity page title"
         />
       </h1>
-      <div className="buttons has-addons">
-        <NavLink
-          to={`${url}/all`}
-          className="button"
-          activeClassName="is-link is-selected"
-        >
-          <span>
-            <FormattedMessage
-              defaultMessage="All"
-              description="All activity tab"
-            />
-          </span>
-        </NavLink>
-        <NavLink
-          to={`${url}/pending`}
-          className="button"
-          activeClassName="is-link is-selected"
-        >
-          <span>
-            <FormattedMessage
-              defaultMessage="Pending"
-              description="Pending activity tab"
-            />
-          </span>
-        </NavLink>
+      <div className="tabs is-boxed">
+        <ul>
+          <NavTab to={`${url}/history`}>
+            <span className="icon is-small">
+              <i className="fas fa-id-badge" aria-hidden="true"></i>
+            </span>
+            <span>
+              <FormattedMessage
+                defaultMessage="History"
+                description="All activity tab"
+              />
+            </span>
+          </NavTab>
+          <NavTab to={`${url}/open`}>
+            <span className="icon is-small">
+              <i className="fas fa-id-badge" aria-hidden="true"></i>
+            </span>
+            <span>
+              <FormattedMessage
+                defaultMessage="My open items"
+                description="All activity tab"
+              />
+            </span>
+            <span className="tag is-link is-light is-rounded ml-2">
+              <FormattedNumber value={openCount} />
+            </span>
+          </NavTab>
+          <NavTab to={`${url}/pending`}>
+            <span className="icon is-small">
+              <i className="fas fa-id-badge" aria-hidden="true"></i>
+            </span>
+            <span>
+              <FormattedMessage
+                defaultMessage="Pending others"
+                description="All activity tab"
+              />
+            </span>
+            <span className="tag is-link is-light is-rounded ml-2">
+              <FormattedNumber value={pendingCount} />
+            </span>
+          </NavTab>
+        </ul>
       </div>
       <Switch>
-        <Route path={`${path}/all`}>
-          <ActivityTable />
+        <Route path={`${path}/history`}>
+          <ActivityTable type="history" />
+        </Route>
+        <Route path={`${path}/open`}>
+          <ActivityTable type="open" />
         </Route>
         <Route path={`${path}/pending`}>
-          <ActivityTable pending />
+          <ActivityTable type="pending" />
         </Route>
         <Route path="*">
-          <Redirect to={`${path}/all`} />
+          <Redirect to={`${path}/history`} />
         </Route>
       </Switch>
     </section>
--- src/features/activity/ActivityTable.js
+++ src/features/activity/ActivityTable.js
@@ -2,8 +2,43 @@ import { useActivity } from '@tokenized/sdk-react-private';
 import React from 'react';
 import { ActivityRow, ActivityHeader } from './ActivityRow';
 
-export default function ActivityTable({ pending }) {
-  const activity = useActivity({ pending });
+export default function ActivityTable({ type }) {
+  let filters;
+  switch (type) {
+    case 'history':
+      filters = {
+        includeSuccessfulEvents: true,
+        includeIncompleteEvents: true,
+        includeFailedEvents: true,
+        includeExpiredEvents: true,
+        includeEventsRequiringAction: false,
+        includeEventsPendingOthers: false,
+      };
+      break;
+    case 'open':
+      filters = {
+        includeSuccessfulEvents: false,
+        includeIncompleteEvents: false,
+        includeFailedEvents: false,
+        includeExpiredEvents: false,
+        includeEventsRequiringAction: true,
+        includeEventsPendingOthers: false,
+      };
+      break;
+    case 'pending':
+      filters = {
+        includeSuccessfulEvents: false,
+        includeIncompleteEvents: false,
+        includeFailedEvents: false,
+        includeExpiredEvents: false,
+        includeEventsRequiringAction: false,
+        includeEventsPendingOthers: true,
+      };
+      break;
+    default:
+      break;
+  }
+  const activity = useActivity(filters);
 
   if (activity?.isLoading) {
     return <progress className="progress is-small is-primary mt-5" max="100" />;
@@ -17,7 +52,7 @@ export default function ActivityTable({ pending }) {
         </thead>
         <tbody>
           {activity?.data?.map((activityItem) => (
-            <ActivityRow key={activityItem.txId} item={activityItem} />
+            <ActivityRow key={activityItem.id} item={activityItem} />
           ))}
         </tbody>
       </table>
